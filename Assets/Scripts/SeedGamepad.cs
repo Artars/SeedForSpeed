c3ab9c0a0cbfb6dd17313008fcfa96caf46d393c
Arthur Souza <arsbr.souza@gmail.com>
2019-09-29 00:41:43 -0300

    Added player controllers

---
+++ Assets/Scripts/SeedGamepad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedGamepad : HFTGamepad
+{
+    [System.Serializable]
+    public class ButtonAction 
+    {
+        public string action0;
+        public string action1;
+
+        public ButtonAction(string action0, string action1)
+        {
+            this.action0 = action0;
+            this.action1 = action1;
+        }
+    }
+
+    public void SetActions(string action0, string action1)
+    {
+        m_netPlayer.SendCmd("changeAction", new ButtonAction(action0,action1));
+    }
+
+    public void SendColor(Color color)
+    {
+        this.Color = color;
+        SendColor();
+    }
+}
